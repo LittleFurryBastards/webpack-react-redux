43a6a37a255cf5a660a4ed8b3b50b836d659cf82
Deyan Yanakiev <kliomba@abv.bg>
2016-05-14 12:08:54 +0300

    Extract shallow rendering in util module

--- __tests__/components/Thumbnail-test.js
+++ __tests__/components/Thumbnail-test.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import ReactTestUtils from 'react-addons-test-utils';
-import Thumbnail from '../../src/components/Thumbnail';
+import shallowRender from '../utils/shallowRender';
 
+import Thumbnail from '../../src/components/Thumbnail';
 jest.unmock('../../src/components/Thumbnail');
 
 describe('Thumbnail Component', () => {
   it('should be a div HTML element', () => {
-    const renderer = ReactTestUtils.createRenderer();
-
-    renderer.render(<Thumbnail />);
-    const output = renderer.getRenderOutput();
+    const output = shallowRender(<Thumbnail />);
 
     expect(output.type).toBe('div');
   });
 
   it('should have a cssName of wrr-thumbnail', () => {
-    const renderer = ReactTestUtils.createRenderer();
-
-    renderer.render(<Thumbnail />);
-    const output = renderer.getRenderOutput();
+    const output = shallowRender(<Thumbnail />);
 
     expect(output.props.className).toBe('wrr-thumbnail');
   });
