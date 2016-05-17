8cedbfa83ef21770931f9466cd4cef835b124fee
Deyan Yanakiev <kliomba@abv.bg>
2016-05-17 17:44:11 +0300

    Update test for actions

--- __tests__/actions.test.js
+++ __tests__/actions.test.js
@@ -2,29 +2,32 @@ jest.unmock('../src/actions');
 
 import axios from 'axios';
 import * as actions from '../src/actions';
-// import store from '../src/store';
+import store from '../src/store';
 
 describe('Actions', () => {
-  // afterEach(() => {
-  //   axios.get.mock.calls = [];
-  //   store.dispatch.mock.calls = [];
-  // });
-
-  it('getUsers should send one get request', () => {
-    actions.getUsers();
-
-    expect(axios.get.mock.calls.length).toBe(1); // eslint-disable-line
+  afterEach(() => {
+    axios.get.mockClear();
+    store.dispatch.mockClear();
   });
 
-  // it('getUsers should dispatch an users:get action on success', (done) => {
-  //   actions.getUsers()
-  //     .then(() => {
-  //       expect(store.dispatch.mock.calls.length).toBe(1); // eslint-disable-line
-  //       expect(store.dispatch.mock.calls.length).toBeCalledWith({
-  //         type: 'users:get',
-  //         data: jasmine.any(Array)
-  //       });
-  //       done();
-  //     });
-  // });
+  it('getUsers should send one get request', (done) => {
+    actions.getUsers()
+      .then(() => {
+        expect(axios.get.mock.calls.length).toBe(1); // eslint-disable-line
+        done();
+      });
+  });
+
+  it('getUsers should dispatch an users:get action on success', (done) => {
+    actions.getUsers()
+      .then(() => {
+        expect(store.dispatch.mock.calls.length).toBe(1); // eslint-disable-line
+        expect(store.dispatch).toBeCalledWith({
+          type: 'users:get',
+          data: jasmine.any(Array)
+        });
+        done();
+      });
+
+  });
 });
